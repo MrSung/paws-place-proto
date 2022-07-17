7d325423e433c7dda117e3efe74b6ccd0a6c3c7f
Sungjoon Park <sungjoon512@gmail.com>
2022-07-17 14:52:59 +0900

    use mantine normalize

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'modern-normalize/modern-normalize.css'
+import { MantineProvider } from '@mantine/core'
 
 import { App } from './app'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 )
