96661ecc0dc07094e68bcd8c344517bd0f8566ef
Sungjoon Park <sungjoon512@gmail.com>
2022-07-17 13:42:40 +0900

    format style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'modern-normalize/modern-normalize.css'
 
-import { App } from "./app";
+import { App } from './app'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
